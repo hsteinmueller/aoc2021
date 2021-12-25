5f35e00329b3fef953d4521b7cb9c76614576121
hsteinmueller <harald.steinmueller@gmail.com>
2021-12-25 18:43:32 +0100

    day 25

--- 25/25.py
+++ 25/25.py
@@ -1,21 +1,66 @@
-import heapq
-import math
-import numpy as np
-import sys
 
-from collections import Counter, deque, defaultdict
-from functools import reduce
-from itertools import groupby
-from operator import mul
-from queue import Queue
 
-ll = [x for x in open("25.ex").read().strip()]
-# ll = [x for x in open("25.in").read().strip()]
+def print_board(D, R, X, Y):
+    for y in range(Y):
+        for x in range(X):
+            if (x, y) in D:
+                print('v', end='')
+            elif (x, y) in R:
+                print('>', end='')
+            else:
+                print('.', end='')
+        print()
 
-ans = 0
-D = {}
 
-for l in ll:
-    pass
+# I = [x for x in open("25.ex").read().strip().split("\n")]
+I = [x for x in open("25.in").read().strip().split("\n")]
 
-print(f'{ans=}')
+X = 0
+Y = 0
+D = set()
+R = set()
+for y, line in enumerate(I):
+    for x, sym in enumerate(line):
+        if sym == '>':
+            R.add((x, y))
+        if sym == 'v':
+            D.add((x, y))
+        X = max(X, x)
+    Y = max(Y, y)
+
+X += 1
+Y += 1
+
+print_board(D, R, X, Y)
+
+steps = 0
+did_move = True
+while did_move:
+    steps += 1
+
+    did_move = False
+
+    RN = set()
+    for r in R:
+        nr = ((r[0] + 1) % X, r[1])
+        if not (nr in R or nr in D):
+            RN.add(nr)
+            did_move = True
+        else:
+            RN.add(r)
+    R = RN
+
+    DN = set()
+    for d in D:
+        nd = (d[0], (d[1] + 1) % Y)
+        if not (nd in R or nd in D):
+            DN.add(nd)
+            did_move = True
+        else:
+            DN.add(d)
+    D = DN
+
+    print(f"step: {steps}")
+
+print_board(D, R, X, Y)
+print(f"{steps=}")
